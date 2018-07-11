3cb11bcb3da2a098e31dc9ba3a012f8f43b907a6
EduardaGoulart <goularteduarda.a@gmail.com>
2018-07-11 15:43:05 -0300

    Class, criando minha view

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.views.generic.base import TemplateView
+from django.http import HttpResponse
+from django.views import View
 
 
 def calcular(v1, v2):
@@ -25,5 +27,14 @@ class HomePageView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['minha_variavel'] = 'Olá, seja bem vindo!!'
-
         return context
+
+
+class MyView(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home3.html')
+
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('Post')
