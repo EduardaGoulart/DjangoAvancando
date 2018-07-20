9167058e07d966795de2671d8f05873013e6ddda
EduardaGoulart <goularteduarda.a@gmail.com>
2018-07-20 10:37:47 -0300

    personalizando o Django Admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 from .models import Person, Documento, Venda, Produto
 
 
-admin.site.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    # Campos exibidos no admin
+    # fields = ('first_name', 'last_name', 'age', 'salary', 'bio', 'photo', 'doc',)
+    # Exibe todos os campos, exceto os que estão nesta lista
+    exclude = ['bio', ]
+    # Exibe um grid com os campos selecionados. O primeiro field é o link
+    list_display = ('first_name', 'last_name', 'age', 'salary', 'bio', 'photo', 'doc',)
+
+
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Documento)
 admin.site.register(Venda)
 admin.site.register(Produto)
