2caffff921312ae95e6e525e5ab9ea128b56d624
EduardaGoulart <goularteduarda.a@gmail.com>
2018-07-23 18:11:57 -0300

    usando funções no dashboard:min, max, count e avg

--- vendas/views.py
+++ vendas/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.views import View
-from django.db.models import Sum, F, FloatField, Max, Avg
+from django.db.models import Max, Avg, Min, Count
 from .models import Venda
 
 
 class DashboardView(View):
     def get(self, request):
+
         media = Venda.objects.all().aggregate(Avg('valor'))['valor__avg']
+        media_desconto = Venda.objects.all().aggregate(Avg('desconto'))['desconto__avg']
+        min = Venda.objects.all().aggregate(Min('valor'))['valor__min']
+        max = Venda.objects.all().aggregate(Max('valor'))['valor__max']
+        n_ped = Venda.objects.all().aggregate(Count('valor'))['valor__count']
+
         context = {
             'media': media,
+            'media_desconto': media_desconto,
+            'min': min,
+            'max': max,
+            'n_ped': n_ped,
         }
         return render(request, 'vendas/dashboard.html', context)
